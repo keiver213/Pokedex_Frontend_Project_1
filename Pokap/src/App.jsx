0a0ad169b817f3fd977a1b1c46b18d4f256e0354
Key <127407446+keiver213@users.noreply.github.com>
2026-04-30 17:49:01 -0500

    Fix import and update routes in App component
    Updated import statement for PokemonDetail to match file naming convention and commented out the Form route.

--- Pokap/src/App.jsx
+++ Pokap/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import PokemonDetail from "./pages/PokemonDetail";
+import PokemonDetail from "./pages/pokemonDetail";
 import Favorites from "./pages/Favorites";
-import Form from "./pages/Form";
+//import Form from "./pages/Form";
 import { Toaster } from "react-hot-toast";
 import NotFound404 from "./pages/404";
 
@@ -17,7 +17,6 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/pokemon/:name" element={<PokemonDetail />} />
         <Route path="/favorites" element={<Favorites />} />
-        <Route path="/form" element={<Form />} />
         <Route path="*" element={<NotFound404 />} />
       </Routes>
     </BrowserRouter>
